d92c689e424bd26153c53fe4fc18d1da88099484
koyaaoyama <aoyama.ko0920@gmail.com>
2023-07-16 14:57:41 -0700

    Use env values in docker-compose.yml and datasource

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,11 +4,11 @@ import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.PGHOST || "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "postgres",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [User],
