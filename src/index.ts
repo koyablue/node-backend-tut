d81bf9003bf8f9fa24e1f4b9d3ac45c68dbb07da
koyaaoyama <aoyama.ko0920@gmail.com>
2023-07-16 15:28:46 -0700

    Add error handling, logging

--- src/index.ts
+++ src/index.ts
@@ -5,30 +5,37 @@ import * as dotenv from "dotenv"
 import { AppDataSource } from "./data-source"
 import { Routes } from "./routes"
 import { User } from "./entity/User"
+import * as morgan from "morgan"
 
 dotenv.config()
+
+const handleError = (err, req, res, next) => {
+  res.status(err.statusCode || 500).send({message: err.message})
+}
 
 AppDataSource.initialize().then(async () => {
 
   // create express app
   const app = express()
+  app.use(morgan('tiny'))
   app.use(bodyParser.json())
 
   // register express routes from defined application routes
   Routes.forEach(route => {
-    (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-      const result = (new (route.controller as any))[route.action](req, res, next)
-      if (result instanceof Promise) {
-        result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
-
-      } else if (result !== null && result !== undefined) {
+    (app as any)[route.method](route.route, async (req: Request, res: Response, next: Function) => {
+      try {
+        const result = await (new (route.controller as any))[route.action](req, res, next)
         res.json(result)
+      } catch (error) {
+        next(error)
       }
     })
   })
 
   // setup express app here
   // ...
+
+  app.use(handleError)
 
   // start express server
   app.listen(Number(process.env.PORT))
