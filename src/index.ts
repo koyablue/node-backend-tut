e62d366cbcaa11bb77077ef2419e32151a776030
koyaaoyama <aoyama.ko0920@gmail.com>
2023-07-16 15:51:27 -0700

    Add request validation

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { AppDataSource } from "./data-source"
 import { Routes } from "./routes"
 import { User } from "./entity/User"
 import * as morgan from "morgan"
+import { validationResult } from "express-validator"
 
 dotenv.config()
 
@@ -22,8 +23,12 @@ AppDataSource.initialize().then(async () => {
 
   // register express routes from defined application routes
   Routes.forEach(route => {
-    (app as any)[route.method](route.route, async (req: Request, res: Response, next: Function) => {
+    (app as any)[route.method](route.route, ...route.validation, async (req: Request, res: Response, next: Function) => {
       try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() })
+        }
         const result = await (new (route.controller as any))[route.action](req, res, next)
         res.json(result)
       } catch (error) {
